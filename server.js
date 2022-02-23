90d988558ff93b07aa4dd8071b9ca9e089ffde04
truth4you <86637147+truth4you@users.noreply.github.com>
2022-02-24 03:27:48 +0900

    https

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+var http = require('http')
+const https = require('https')
 const next = require('next')
 const app = express()
+const fs = require('fs')
 
 const account = require('./backend/account')
 const competition = require('./backend/competition')
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
 
 const port = process.argv[3]
 const server = next({ dev:process.argv[4] && process.argv[4]==='-dev', port })
@@ -19,8 +27,21 @@ server.prepare().then(() => {
 	app.all('*', (req, res) => {
     return handle(req, res)
   })
-	app.listen(port, (err) => {
-		if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-	})
+	var httpServer = http.createServer(app);
+	var httpsServer = https.createServer(options, app)
+	if(port==80 || port==443) {
+		httpServer.listen(80, (err) => {
+			if (err) throw err
+			console.log(`> Ready on http://localhost`)
+		})
+		httpsServer.listen(443, (err) => {
+			if (err) throw err
+			console.log(`> Ready on https://localhost:${port}`)
+		})
+	} else {
+		httpsServer.listen(port, (err) => {
+			if (err) throw err
+			console.log(`> Ready on https://localhost:${port}`)
+		})
+	}
 })
