2e4140ac89ebceebbcdcd8714a72721389eee0ce
webdev <liuzhemin0305@gmail.com>
2022-05-07 07:53:28 +0900

    second

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const fs = require('fs')
 const account = require('./backend/account')
 const competition = require('./backend/competition')
 
-// const options = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// }
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
 
 const port = 3000;
 const server = next({ dev:process.argv[4] && process.argv[4]==='-dev', port })
@@ -28,20 +28,20 @@ server.prepare().then(() => {
     return handle(req, res)
   })
 	var httpServer = http.createServer(app);
-	// var httpsServer = https.createServer(options, app)
-	// if(port==80 || port==443) {
+	var httpsServer = https.createServer(options, app)
+	if(port==80 || port==443) {
 		httpServer.listen(80, (err) => {
 			if (err) throw err
 			console.log(`> Ready on http://localhost`)
 		})
-	// 	httpsServer.listen(443, (err) => {
-	// 		if (err) throw err
-	// 		console.log(`> Ready on https://localhost`)
-	// 	})
-	// } else {
-	// httpsServer.listen(port, (err) => {
-	// 	if (err) throw err
-	// 	console.log(`> Ready on https://localhost:${port}`)
-	// })
-	// }
+		httpsServer.listen(443, (err) => {
+			if (err) throw err
+			console.log(`> Ready on https://localhost`)
+		})
+	} else {
+	httpsServer.listen(port, (err) => {
+		if (err) throw err
+		console.log(`> Ready on https://localhost:${port}`)
+	})
+	}
 })
