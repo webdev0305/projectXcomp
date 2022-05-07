b84cb7283f6c664fa79c3d6b002294c55a30f70a
webdev <liuzhemin0305@gmail.com>
2022-05-07 19:47:04 +0900

    fixed

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,33 +1,33 @@
-import Collapse from "../components/Collaps"
+import Collapse from "components/Collaps"
 
-const faq = [
-    {
-        question: 'Question one',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    },
-    {
-        question: 'Question two',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    },
-    {
-        question: 'Question three',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    },
-    {
-        question: 'Question four',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    },
-    {
-        question: 'Question five',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    },
-    {
-        question: 'Question six',
-        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
-    }
-]
+
 export default function Faq() {
-
+    const faq = [
+        {
+            question: 'Question one',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        },
+        {
+            question: 'Question two',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        },
+        {
+            question: 'Question three',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        },
+        {
+            question: 'Question four',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        },
+        {
+            question: 'Question five',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        },
+        {
+            question: 'Question six',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra  maecenas accumsan lacus vel facilisis.'
+        }
+    ];
     return (
         <div className="page-wrapper">
             <div className="inner-hero-section style--five">
@@ -45,78 +45,13 @@ export default function Faq() {
                                     <div className="tab-pane fade show active" role="tabpanel">
                                         <div className="accordion cmn-accordion">
                                             {faq.map((item:any)=>
-                                                <Collapse 
-                                                    question={item.question}
-                                                    answer={item.answer}
-                                                />
-                                                
+                                                <div className="my-4" key={item.question}>
+                                                    <Collapse 
+                                                        question={item.question}
+                                                        answer={item.answer}
+                                                    />
+                                                </div>
                                             )}
-                                            
-
-                                            <div className="card">
-                                                <div className="card-header" id="h-2">
-                                                    <button className="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                                                        Question two
-                                                    </button>
-                                                </div>
-                                                <div id="collapse2" className="collapse" aria-labelledby="h-2" data-parent="#faqAcc-one">
-                                                    <div className="card-body">
-                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div className="card">
-                                                <div className="card-header" id="h-3">
-                                                    <button className="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                                                        Question three
-                                                    </button>
-                                                </div>
-                                                <div id="collapse3" className="collapse" aria-labelledby="h-3" data-parent="#faqAcc-one">
-                                                    <div className="card-body">
-                                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a debitis, obcaecati beatae possimus veniam distinctio?</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div className="card">
-                                                <div className="card-header" id="h-4">
-                                                    <button className="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
-                                                        Question four
-                                                    </button>
-                                                </div>
-                                                <div id="collapse4" className="collapse" aria-labelledby="h-4" data-parent="#faqAcc-one">
-                                                    <div className="card-body">
-                                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a debitis, obcaecati beatae possimus veniam distinctio?</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div className="card">
-                                                <div className="card-header" id="h-5">
-                                                    <button className="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
-                                                        Question five
-                                                    </button>
-                                                </div>
-                                                <div id="collapse5" className="collapse" aria-labelledby="h-5" data-parent="#faqAcc-one">
-                                                    <div className="card-body">
-                                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a debitis, obcaecati beatae possimus veniam distinctio?</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div className="card">
-                                                <div className="card-header" id="h-6">
-                                                    <button className="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
-                                                        Question six
-                                                    </button>
-                                                </div>
-                                                <div id="collapse6" className="collapse" aria-labelledby="h-6" data-parent="#faqAcc-one">
-                                                    <div className="card-body">
-                                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum dignissimos consectetur aspernatur expedita aut reiciendis magni tempore ullam libero, voluptate nam accusamus est a debitis, obcaecati beatae possimus veniam distinctio?</p>
-                                                    </div>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
