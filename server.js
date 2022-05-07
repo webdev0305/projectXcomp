c001967447207d4c426c84a7c116bdabb58cd946
webdev <liuzhemin0305@gmail.com>
2022-05-07 13:15:48 +0900

    fifth

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express')
-// var http = require('http')
-// const https = require('https')
 const next = require('next')
 const app = express()
-const fs = require('fs')
-
 const account = require('./backend/account')
 const competition = require('./backend/competition')
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-}
 
 const port = process.argv[3] ?? 3000;
 const server = next({ dev:process.argv[4] && process.argv[4]==='-dev', port })
@@ -25,23 +16,11 @@ app.use('/api/competition', competition)
 
 server.prepare().then(() => {
 	app.all('*', (req, res) => {
-    return handle(req, res)
-  })
-	// var httpServer = http.createServer(app);
-	// var httpsServer = https.createServer(options, app)
-	// if(port==80 || port==443) {
-	// 	httpServer.listen(80, (err) => {
-	// 		if (err) throw err
-	// 		console.log(`> Ready on http://localhost`)
-	// 	})
-	// 	httpsServer.listen(443, (err) => {
-	// 		if (err) throw err
-	// 		console.log(`> Ready on https://localhost`)
-	// 	})
-	// } else {
+		return handle(req, res)
+	})
+
 	app.listen(port, (err) => {
 		if (err) throw err
 		console.log(`> Ready on https://localhost:${port}`)
 	})
-	// }
 })
