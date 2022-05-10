af4ff847291072c29279b9dc3e3f0799926652bc
webdev <liuzhemin0305@gmail.com>
2022-05-10 17:33:08 +0900

    delete ui

--- NEW_UI_DESIGN_REFERENCE/React Version/src/Components/Home.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import styled from "styled-components";
-const Wrapper = styled.div``;
-const Home = () => {
-  return <Wrapper></Wrapper>;
-};
-export default Home;
